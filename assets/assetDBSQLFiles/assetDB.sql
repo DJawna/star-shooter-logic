b6b3e898322c2e7e52f014ce39570bd50474c4e4
daniel <djawna@gmail.com>
2020-08-16 12:19:27 +0200

    rename column from scene to screen

--- assets/assetDBSQLFiles/assetDB.sql
+++ assets/assetDBSQLFiles/assetDB.sql
@@ -1,22 +1,14 @@
 BEGIN TRANSACTION;
-CREATE TABLE IF NOT EXISTS "global_controls" (
-	"inputName"	TEXT NOT NULL,
-	"value"	INTEGER NOT NULL,
-	"defaultValue"	INTEGER NOT NULL,
-	PRIMARY KEY("inputName"),
-	FOREIGN KEY("defaultValue") REFERENCES "global_keymappings"("keyCode"),
-	FOREIGN KEY("value") REFERENCES "global_keymappings"("keyCode")
+CREATE TABLE IF NOT EXISTS "global_gui_elements" (
+	"screen_id"	INTEGER NOT NULL,
+	"id"	INTEGER NOT NULL,
+	PRIMARY KEY("screen_id","id"),
+	FOREIGN KEY("screen_id") REFERENCES "global_screen"("screen_id")
 );
-CREATE TABLE IF NOT EXISTS "global_keymappings" (
-	"keyCode"	INTEGER NOT NULL,
-	"keyName"	TEXT NOT NULL,
-	PRIMARY KEY("keyCode")
-);
-CREATE TABLE IF NOT EXISTS "global_audiosettings" (
-	"audioSetting"	TEXT NOT NULL,
-	"value"	INTEGER NOT NULL CHECK(value<=100 and value>=0),
-	"defaultValue"	INTEGER NOT NULL CHECK(defaultValue<=100 and defaultValue>=0),
-	PRIMARY KEY("audioSetting")
+CREATE TABLE IF NOT EXISTS "global_screens" (
+	"screen_id"	INTEGER NOT NULL,
+	"screen_name"	TEXT NOT NULL,
+	PRIMARY KEY("screen_id")
 );
 CREATE TABLE IF NOT EXISTS "global_resolution_settings" (
 	"settingname"	TEXT NOT NULL,
@@ -24,15 +16,40 @@ CREATE TABLE IF NOT EXISTS "global_resolution_settings" (
 	"defaultValue"	INTEGER NOT NULL CHECK(defaultValue>=0),
 	PRIMARY KEY("settingname")
 );
-INSERT INTO "global_keymappings" ("keyName","keyCode") VALUES ('cursor_up',1),
- ('cursor_left',2),
- ('cursor_right',3),
- ('cursor_down',4),
- ('space',5),
- ('control',6);
+CREATE TABLE IF NOT EXISTS "global_audiosettings" (
+	"audioSetting"	TEXT NOT NULL,
+	"value"	INTEGER NOT NULL CHECK(value<=100 and value>=0),
+	"defaultValue"	INTEGER NOT NULL CHECK(defaultValue<=100 and defaultValue>=0),
+	PRIMARY KEY("audioSetting")
+);
+CREATE TABLE IF NOT EXISTS "global_keymappings" (
+	"keyCode"	INTEGER NOT NULL,
+	"keyName"	TEXT NOT NULL,
+	PRIMARY KEY("keyCode")
+);
+CREATE TABLE IF NOT EXISTS "global_controls" (
+	"inputName"	TEXT NOT NULL,
+	"value"	INTEGER NOT NULL,
+	"defaultValue"	INTEGER NOT NULL,
+	FOREIGN KEY("value") REFERENCES "global_keymappings"("keyCode"),
+	FOREIGN KEY("defaultValue") REFERENCES "global_keymappings"("keyCode"),
+	PRIMARY KEY("inputName")
+);
+INSERT INTO "global_resolution_settings" ("settingname","value","defaultValue") VALUES ('resX',800,800),
+ ('resY',600,600);
 INSERT INTO "global_audiosettings" ("audioSetting","value","defaultValue") VALUES ('SoundEffectsVolume',100,100),
  ('musicVolume',100,100),
  ('masterVolume',100,100);
-INSERT INTO "global_resolution_settings" ("settingname","value","defaultValue") VALUES ('resX',800,800),
- ('resY',600,600);
+INSERT INTO "global_keymappings" ("keyCode","keyName") VALUES (1,'cursor_up'),
+ (2,'cursor_left'),
+ (3,'cursor_right'),
+ (4,'cursor_down'),
+ (5,'space'),
+ (6,'control');
+INSERT INTO "global_controls" ("inputName","value","defaultValue") VALUES ('up',1,1),
+ ('left',2,2),
+ ('right',3,3),
+ ('down',4,4),
+ ('fire',5,5),
+ ('special_item',6,6);
 COMMIT;
